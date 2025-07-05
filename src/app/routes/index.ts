ad86ba2c7d4b8b8050951df86614da88833ecfc8
Ivan José Mejias <ivan.mejias@kairosds.com>
2025-07-05 19:55:33 +0200

    feat: create scaffold folder structure and main route

---
+++ src/app/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+export default function mainRouter() {
+  const router: Router = Router();
+
+  router.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+
+  return router;
+}
