f316ac51dddac851c466c5fdc700527c1a83820e
Ivan José Mejias <ivan.mejias@kairosds.com>
2025-07-06 18:22:58 +0200

    feat: create Models

---
+++ src/app/models/TaskModel.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export enum TaskStatus {
+  COMPLETED = 'completed',
+  PENDING = 'pending',
+  ERROR = 'error',
+}
+
+interface ITask extends Document {
+  status: string;
+  price: number;
+  originalPath: string;
+  images?: Array<{
+    resolution: string;
+    path: string;
+  }>;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const TaskSchema: Schema = new Schema(
+  {
+    status: {
+      type: String,
+      enum: TaskStatus,
+      required: true,
+    },
+    price: { type: Number, required: true },
+    originalPath: { type: String, required: true },
+    images: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'images',
+      },
+    ],
+  },
+  { timestamps: true, versionKey: false },
+);
+
+const TaskModel = mongoose.model<ITask>('tasks', TaskSchema);
+export { TaskModel, ITask };
