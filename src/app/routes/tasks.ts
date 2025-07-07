dcae12c57b9afad9bc74473677eed01e5f70297f
Ivan José Mejias <ivan.mejias@kairosds.com>
2025-07-07 23:42:00 +0200

    feat: apply validators to routes

--- src/app/routes/tasks.ts
+++ src/app/routes/tasks.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { createTask, getTaskById } from '../controllers/tasks';
+import { taskGetValidator } from '../validators/taskGet.validator';
+import { taskPostValidator } from '../validators/taskPost.validator';
 
 export default function tasksRoutes() {
   const router: Router = Router();
 
-  router.post('/tasks', createTask);
-  router.get('/tasks/:id', getTaskById);
+  router.post('/tasks', taskPostValidator, createTask);
+  router.get('/tasks/:id', taskGetValidator, getTaskById);
 
   return router;
 }
